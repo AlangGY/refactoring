bebad79317878c361eea124f46df0acae88ff745
Alang Kim <rlaangh77@naver.com>
2022-05-22 20:26:51 +0900

    🐛 fix: performanceCalculator 서브클래스 선언 위치 수정

--- CH1/createStatementData.js
+++ CH1/createStatementData.js
@@ -31,21 +31,6 @@ export default function createStatementData(invoice, plays) {
   }
 }
 
-function createPerformanceCalculator(aPerformance, aPlay) {
-  switch (aPlay.type) {
-    case "tragedy":
-      return new TragedyCalculator(aPerformance, aPlay);
-    case "comedy":
-      return new ComedyCalculator(aPerformance, aPlay);
-    default:
-      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
-  }
-}
-
-class TragedyCalculator extends PerformanceCalculator {}
-
-class ComedyCalculator extends PerformanceCalculator {}
-
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
@@ -81,5 +66,20 @@ class PerformanceCalculator {
       result += Math.floor(this.performance.audience / 5);
 
     return result;
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {}
+
+class ComedyCalculator extends PerformanceCalculator {}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
   }
 }
