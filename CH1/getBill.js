6a1b58fb8df6f6ac7005a819df9aeebeca7c3712
Alang Kim <rlaangh77@naver.com>
2022-05-22 16:12:56 +0900

    ♻️ refactor: 단위 변환 로직 usd 함수 내부로 이동

--- CH1/getBill.js
+++ CH1/getBill.js
@@ -7,12 +7,12 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역 출력
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${usd(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
@@ -21,7 +21,7 @@ function statement(invoice, plays) {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 
   function volumeCreditsFor(aPerformance) {
