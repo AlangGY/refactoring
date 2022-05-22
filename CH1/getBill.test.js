741186fa3a73330ea3df6221ef77dd63955cf250
Alang Kim <rlaangh77@naver.com>
2022-05-22 19:43:45 +0900

    🧪 test: htmlStatement test code추가

--- CH1/getBill.test.js
+++ CH1/getBill.test.js
@@ -1,4 +1,4 @@
-import { statement } from "./getBill.js";
+import { htmlStatement, statement } from "./getBill.js";
 import invoices from "./invoices.json";
 import plays from "./plays.json";
 
@@ -23,6 +23,86 @@ const properBill = `청구 내역 (고객명: BigCo)
 총액: $1,715.00
 적립 포인트: 61점`;
 
+const properBillHTML = `<h1>청구 내역 (고객명: BigCo)</h1>
+
+    <table>
+        <tr>
+            <th>연극</th>
+            <th>좌석 수</th>
+            <th>금액</th>
+        </tr>
+        <tr>
+                <td>Hamlet</td>
+                <td>(55석)</td>
+                <td>$650.00</td>
+            </tr>
+            <tr>
+                <td>As You Like It</td>
+                <td>(35석)</td>
+                <td>$580.00</td>
+            </tr>
+            <tr>
+                <td>Othello</td>
+                <td>(40석)</td>
+                <td>$500.00</td>
+            </tr>
+            
+    </table>
+    <p>총액: <em>$1,730.00</em></p>
+<p>적립 포인트: <em>47</em>점</p><h1>청구 내역 (고객명: Alang)</h1>
+
+    <table>
+        <tr>
+            <th>연극</th>
+            <th>좌석 수</th>
+            <th>금액</th>
+        </tr>
+        <tr>
+                <td>Hamlet</td>
+                <td>(20석)</td>
+                <td>$400.00</td>
+            </tr>
+            <tr>
+                <td>As You Like It</td>
+                <td>(70석)</td>
+                <td>$860.00</td>
+            </tr>
+            <tr>
+                <td>Othello</td>
+                <td>(40석)</td>
+                <td>$500.00</td>
+            </tr>
+            
+    </table>
+    <p>총액: <em>$1,760.00</em></p>
+<p>적립 포인트: <em>64</em>점</p><h1>청구 내역 (고객명: jeong woon)</h1>
+
+    <table>
+        <tr>
+            <th>연극</th>
+            <th>좌석 수</th>
+            <th>금액</th>
+        </tr>
+        <tr>
+                <td>Hamlet</td>
+                <td>(90석)</td>
+                <td>$1,000.00</td>
+            </tr>
+            <tr>
+                <td>As You Like It</td>
+                <td>(5석)</td>
+                <td>$315.00</td>
+            </tr>
+            <tr>
+                <td>Othello</td>
+                <td>(10석)</td>
+                <td>$400.00</td>
+            </tr>
+            
+    </table>
+    <p>총액: <em>$1,715.00</em></p>
+<p>적립 포인트: <em>61</em>점</p>`;
+
 describe("getBill", () => {
   test("invoices.json and plays.json will return bill properly", () => {
     let result = "";
@@ -32,5 +112,13 @@ describe("getBill", () => {
 
     result = result.trimEnd();
     expect(result).toBe(properBill);
+  });
+
+  test("rendering HTML", () => {
+    let result = "";
+    for (const invoice of invoices) {
+      result += htmlStatement(invoice, plays);
+    }
+    expect(result).toBe(properBillHTML);
   });
 });
