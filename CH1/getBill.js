cb282cf07ae8ba6183f5ee7dcc064a9cfa7b60fc
Alang Kim <rlaangh77@naver.com>
2022-05-22 15:50:23 +0900

    ♻️ refactor: perf 변수명 명확도 높이기

--- CH1/getBill.js
+++ CH1/getBill.js
@@ -9,7 +9,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
 
     let thisAmount = amountFor(perf, play);
 
@@ -28,27 +28,31 @@ function statement(invoice, plays) {
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
-  function amountFor(perf, play) {
-    let thisAmount = 0;
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance, play) {
+    let result = 0;
     switch (play.type) {
       case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
         }
         break;
       case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
         }
-        thisAmount += 300 * perf.audience;
+        result += 300 * aPerformance.audience;
         break;
       default:
         throw new Error(`알수 없는 장르: ${play.type}`);
     }
 
-    return thisAmount;
+    return result;
   }
 }
 
