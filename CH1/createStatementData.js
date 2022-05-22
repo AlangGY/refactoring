3c7231fefe81e943caf7cad99a30f5d1e25bd741
Alang Kim <rlaangh77@naver.com>
2022-05-22 20:31:17 +0900

    ♻️ refactor: amount 메소드 서브도메인으로 이동

--- CH1/createStatementData.js
+++ CH1/createStatementData.js
@@ -40,18 +40,9 @@ class PerformanceCalculator {
     let result = 0;
     switch (this.play.type) {
       case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
+        throw new Error("use TragedyCalculator");
       case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
+        throw new Error("use ComedyCalculator");
       default:
         throw new Error(`알수 없는 장르: ${this.play.type}`);
     }
@@ -69,9 +60,27 @@ class PerformanceCalculator {
   }
 }
 
-class TragedyCalculator extends PerformanceCalculator {}
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
 
-class ComedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+
+    return result;
+  }
+}
 
 function createPerformanceCalculator(aPerformance, aPlay) {
   switch (aPlay.type) {
