0ef34be0ca853ab3883624bd5d3333a8b468341b
Alang Kim <rlaangh77@naver.com>
2022-05-22 18:16:20 +0900

    ♻️ refactor: playFor 함수 뺴내기

--- CH1/getBill.js
+++ CH1/getBill.js
@@ -6,7 +6,12 @@ function statement(invoice, plays) {
 
   function enrichPerformances(aPerformance) {
     const result = { ...aPerformance };
+    result.play = playFor(result);
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 
@@ -15,7 +20,7 @@ function renderPlainText(data, plays) {
 
   for (let perf of data.performances) {
     // 청구 내역 출력
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+    result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
   }
@@ -51,19 +56,15 @@ function renderPlainText(data, plays) {
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type)
+    if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
 
     return result;
   }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -78,7 +79,7 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알수 없는 장르: ${playFor(aPerformance).type}`);
+        throw new Error(`알수 없는 장르: ${aPerformance.play.type}`);
     }
 
     return result;
