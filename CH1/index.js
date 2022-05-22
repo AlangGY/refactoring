2626d2c19e02e7d520fe781a763fd658e5788ff3
Alang Kim <rlaangh77@naver.com>
2022-05-22 19:45:26 +0900

    🐛 fix: esmodule 내에서 require문 사용할수 있도록 수정

--- CH1/index.js
+++ CH1/index.js
@@ -1,6 +1,9 @@
+import { createRequire } from "module";
 import { statement } from "./getBill.js";
-import invoices from "./invoices.json";
-import plays from "./plays.json";
+const require = createRequire(import.meta.url);
+
+const invoices = require("./invoices.json");
+const plays = require("./plays.json");
 
 for (const invoice of invoices) {
   console.log(statement(invoice, plays));
