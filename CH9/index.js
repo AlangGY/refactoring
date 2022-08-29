74b934ed6d453a091d79ab8abb3354610617cca5
JEONG WOON KIM <jkim@iportfolio.co.kr>
2022-08-29 21:41:23 +0900

    feat: 최초 distanceTraveled 함수 작성

---
+++ CH9/index.js
@@ -0,0 +1,16 @@
+function distanceTraveled(scenario, time) {
+  let result;
+  let acc = scenario.primaryForce / scenario.mass; // F = ma_1
+  let primaryTime = Math.min(time, scenario.delay);
+  result = 0.5 * acc * primaryTime * primaryTime; // 1/2 * a * t^2
+  let secondaryTime = time - scenario.delay; // 두번쨰 힘 이후 소요된 시간
+  if (secondaryTime > 0) {
+    let primaryVelocity = acc * scenario.delay;
+    acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass; // (F1 + F2) = ma_2
+    result +=
+      primaryVelocity * secondaryTime +
+      0.5 * acc * secondaryTime * secondaryTime;
+  }
+
+  return result;
+}
