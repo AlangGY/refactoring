0d0c037d6400a9afb7fbf08ca56a05c0c79e6611
JEONG WOON KIM <jkim@iportfolio.co.kr>
2022-08-29 22:12:05 +0900

    refactor: 가속도와 시간으로 거리구하는 공식 함수화

--- CH9/index.js
+++ CH9/index.js
@@ -5,7 +5,7 @@ export function distanceTraveled(
   let result;
   const primaryAcceleration = acceleration(primaryForce, mass);
   const primaryTime = Math.min(time, delay);
-  result = 0.5 * primaryAcceleration * primaryTime * primaryTime; // 1/2 * a * t^2
+  result = distance(primaryAcceleration, primaryTime);
   const secondaryTime = time - delay; // 두번쨰 힘 이후 소요된 시간
   if (secondaryTime > 0) {
     const primaryVelocity = primaryAcceleration * delay;
@@ -15,7 +15,7 @@ export function distanceTraveled(
     );
     result +=
       primaryVelocity * secondaryTime +
-      0.5 * secondaryAcceleration * secondaryTime * secondaryTime;
+      distance(secondaryAcceleration, secondaryTime);
   }
 
   return result;
@@ -23,5 +23,10 @@ export function distanceTraveled(
   function acceleration(force, mass) {
     // a = F / m
     return force / mass;
+  }
+
+  function distance(acceleration, time) {
+    // 1/2 * a * t^2
+    return 0.5 * acceleration * time * time;
   }
 }
