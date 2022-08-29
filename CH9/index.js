e8dea757de55cfa2c9f5b5cd0102a59153d353dd
JEONG WOON KIM <jkim@iportfolio.co.kr>
2022-08-29 22:01:39 +0900

    refactor: 최초 가속도 변수 const화

--- CH9/index.js
+++ CH9/index.js
@@ -1,12 +1,12 @@
 export function distanceTraveled(scenario, time) {
   let result;
-  let acc = scenario.primaryForce / scenario.mass; // F = ma_1
+  const primaryAcceleration = scenario.primaryForce / scenario.mass; // F = ma_1
   let primaryTime = Math.min(time, scenario.delay);
-  result = 0.5 * acc * primaryTime * primaryTime; // 1/2 * a * t^2
+  result = 0.5 * primaryAcceleration * primaryTime * primaryTime; // 1/2 * a * t^2
   let secondaryTime = time - scenario.delay; // 두번쨰 힘 이후 소요된 시간
   if (secondaryTime > 0) {
-    let primaryVelocity = acc * scenario.delay;
-    acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass; // (F1 + F2) = ma_2
+    let primaryVelocity = primaryAcceleration * scenario.delay;
+    let acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass; // (F1 + F2) = ma_2
     result +=
       primaryVelocity * secondaryTime +
       0.5 * acc * secondaryTime * secondaryTime;
