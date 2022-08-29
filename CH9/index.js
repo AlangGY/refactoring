63c524d3a5caf8a1ed0cec96c15e4d34e2654491
JEONG WOON KIM <jkim@iportfolio.co.kr>
2022-08-29 22:03:10 +0900

    refactor: 두번쨰 가속도 변수 상수화

--- CH9/index.js
+++ CH9/index.js
@@ -6,10 +6,11 @@ export function distanceTraveled(scenario, time) {
   let secondaryTime = time - scenario.delay; // 두번쨰 힘 이후 소요된 시간
   if (secondaryTime > 0) {
     let primaryVelocity = primaryAcceleration * scenario.delay;
-    let acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass; // (F1 + F2) = ma_2
+    const secondaryAcceleration =
+      (scenario.primaryForce + scenario.secondaryForce) / scenario.mass; // (F1 + F2) = ma_2
     result +=
       primaryVelocity * secondaryTime +
-      0.5 * acc * secondaryTime * secondaryTime;
+      0.5 * secondaryAcceleration * secondaryTime * secondaryTime;
   }
 
   return result;
