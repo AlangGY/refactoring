2ebcc3e2619fe504c4ff6d59405a6a8207eff564
JEONG WOON KIM <jkim@iportfolio.co.kr>
2022-08-29 22:04:09 +0900

    refactor: 불필요한 let변수 const로 변경

--- CH9/index.js
+++ CH9/index.js
@@ -1,11 +1,11 @@
 export function distanceTraveled(scenario, time) {
   let result;
   const primaryAcceleration = scenario.primaryForce / scenario.mass; // F = ma_1
-  let primaryTime = Math.min(time, scenario.delay);
+  const primaryTime = Math.min(time, scenario.delay);
   result = 0.5 * primaryAcceleration * primaryTime * primaryTime; // 1/2 * a * t^2
-  let secondaryTime = time - scenario.delay; // 두번쨰 힘 이후 소요된 시간
+  const secondaryTime = time - scenario.delay; // 두번쨰 힘 이후 소요된 시간
   if (secondaryTime > 0) {
-    let primaryVelocity = primaryAcceleration * scenario.delay;
+    const primaryVelocity = primaryAcceleration * scenario.delay;
     const secondaryAcceleration =
       (scenario.primaryForce + scenario.secondaryForce) / scenario.mass; // (F1 + F2) = ma_2
     result +=
