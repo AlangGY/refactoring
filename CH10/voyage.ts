67a069e9430ac69a8d6480969bfc1b12210fa160
Alang Kim <rlaangh77@naver.com>
2022-08-30 17:51:38 +0900

    refactor: Rating 각 메소드들 protected로 변경

--- CH10/voyage.ts
+++ CH10/voyage.ts
@@ -44,7 +44,7 @@ class Rating extends RatingAbstract {
     else return "B";
   }
 
-  get voyageRisk() {
+  protected get voyageRisk() {
     // 항해 경로 위험요소
     let result = 1;
     if (this.voyage.length > 4) result += 2;
@@ -53,7 +53,7 @@ class Rating extends RatingAbstract {
     return Math.max(result, 0);
   }
 
-  get captainHistoryRisk() {
+  protected get captainHistoryRisk() {
     // 선장의 항해 이력 위험 요소
     let result = 1;
     if (this.history.length < 5) result += 4;
@@ -61,7 +61,7 @@ class Rating extends RatingAbstract {
     return Math.max(result, 0);
   }
 
-  get voyageProfitFactor() {
+  protected get voyageProfitFactor() {
     // 수익 요인
     let result = 2;
     if (this.voyage.zone === "중국") result += 1;
@@ -71,30 +71,30 @@ class Rating extends RatingAbstract {
     return result;
   }
 
-  get historyLengthFactor() {
+  protected get historyLengthFactor() {
     return this.history.length > 8 ? 1 : 0;
   }
 
-  get voyageLengthFactor(): number {
+  protected get voyageLengthFactor(): number {
     return this.voyage.length > 14 ? -1 : 0;
   }
 }
 
 class ExperiencedChinaRating extends Rating {
-  get captainHistoryRisk() {
+  protected get captainHistoryRisk() {
     const result = super.captainHistoryRisk - 2;
     return Math.max(result, 0);
   }
 
-  get voyageProfitFactor() {
+  protected get voyageProfitFactor() {
     return super.voyageProfitFactor + 3;
   }
 
-  get historyLengthFactor() {
+  protected get historyLengthFactor() {
     return this.history.length > 10 ? 1 : 0;
   }
 
-  get voyageLengthFactor() {
+  protected get voyageLengthFactor() {
     let result = 0;
     if (this.voyage.length > 12) result += 1;
     if (this.voyage.length > 18) result -= 1;
