c2c85678680c10371d9656a8e8f667a4fc0a93b0
Alang Kim <rlaangh77@naver.com>
2022-09-20 00:49:20 +0900

    refacotor: 질의함수 적용 및 기존함수에서 질의 관련 코드 제거

--- CH11/11-1/index.js
+++ CH11/11-1/index.js
@@ -5,14 +5,11 @@ export function alertForMiscreant(people){
     for (const p of people){
         if (p === '조커'){
             setOffAlarms();
-            return '조커'
         }
         if (p === '사루만'){
             setOffAlarms();
-            return '사루만'
         }
     }
-    return '';
 }
 
 export function findMiscreant(people){
--- CH11/__test__/index.test.js
+++ CH11/__test__/index.test.js
@@ -9,19 +9,22 @@ afterEach(() => {
 
 describe('alert for miscreant', () => {
     test('', () => {
-        const returnValue = module.alertForMiscreant(['조커', '사루만', '배트맨']);
+        const returnValue = module.findMiscreant(['조커', '사루만', '배트맨']);
+        module.alertForMiscreant(['조커', '사루만', '배트맨']);
         expect(returnValue).toBe('조커');
         expect(alertModule.setOffAlarms).toHaveBeenCalled()
     });
 
     test('', () => {
-        const returnValue = module.alertForMiscreant([ '사루만','조커', '배트맨']);
+        const returnValue = module.findMiscreant([ '사루만','조커', '배트맨']);
+        module.alertForMiscreant([ '사루만','조커', '배트맨']);
         expect(returnValue).toBe('사루만');
         expect(alertModule.setOffAlarms).toHaveBeenCalled()
     });
 
     test('', () => {
-        const returnValue = module.alertForMiscreant([ '할리퀸','슈퍼맨', '배트맨']);
+        const returnValue = module.findMiscreant([ '할리퀸','슈퍼맨', '배트맨']);
+        module.alertForMiscreant([ '할리퀸','슈퍼맨', '배트맨']);
         expect(returnValue).toBe('');
         expect(alertModule.setOffAlarms).not.toHaveBeenCalled();
     });
