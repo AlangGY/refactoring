0f90d485fb13517a97be13e09b9912ef7efb3001
Alang Kim <rlaangh77@naver.com>
2022-09-20 01:01:07 +0900

    refactor: 로직 파이프라인으로 변경

--- CH11/11-1/index.js
+++ CH11/11-1/index.js
@@ -5,13 +5,5 @@ export function alertForMiscreant(people) {
 }
 
 export function findMiscreant(people) {
-  for (const p of people) {
-    if (p === "조커") {
-      return "조커";
-    }
-    if (p === "사루만") {
-      return "사루만";
-    }
-  }
-  return "";
+  return people.find((p) => ["조커", "사루만"].includes(p)) || "";
 }
