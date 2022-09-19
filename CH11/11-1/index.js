bcf269ffd78a760f8c1636d420fe83b03b50d183
Alang Kim <rlaangh77@naver.com>
2022-09-20 00:52:41 +0900

    refactor: Prettier 적용 및 알고리즘 교체하기 적용

--- CH11/11-1/index.js
+++ CH11/11-1/index.js
@@ -1,25 +1,17 @@
 import { setOffAlarms } from "./alert";
 
-
-export function alertForMiscreant(people){
-    for (const p of people){
-        if (p === '조커'){
-            setOffAlarms();
-        }
-        if (p === '사루만'){
-            setOffAlarms();
-        }
-    }
+export function alertForMiscreant(people) {
+  if (findMiscreant(people)) setOffAlarms();
 }
 
-export function findMiscreant(people){
-    for (const p of people){
-        if (p === '조커'){
-            return '조커'
-        }
-        if (p === '사루만'){
-            return '사루만'
-        }
+export function findMiscreant(people) {
+  for (const p of people) {
+    if (p === "조커") {
+      return "조커";
     }
-    return '';
+    if (p === "사루만") {
+      return "사루만";
+    }
+  }
+  return "";
 }
