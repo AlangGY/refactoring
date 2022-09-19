d3be54ddc08d6ad56a2f20a7838534c69611888d
Alang Kim <rlaangh77@naver.com>
2022-09-20 00:47:17 +0900

    refactor: 질의 함수 분리

--- CH11/11-1/index.js
+++ CH11/11-1/index.js
@@ -14,3 +14,15 @@ export function alertForMiscreant(people){
     }
     return '';
 }
+
+export function findMiscreant(people){
+    for (const p of people){
+        if (p === '조커'){
+            return '조커'
+        }
+        if (p === '사루만'){
+            return '사루만'
+        }
+    }
+    return '';
+}
